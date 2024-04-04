a4ea40d430144c540ba9f13af924b7d11602d1c3
Hassan Chowdhry <94680098+HassanChowdhry@users.noreply.github.com>
2024-04-04 18:39:46 -0300

    init and setup navbar

---
+++ src/components/Hero.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {motion} from 'framer-motion';
+import { styles } from '../style';
+import {ComputerCanvas} from './canvas'
+
+const Hero = () => {
+  return (
+    <section className='relative w-full h-screen mx-auto'>Hero</section>
+  )
+}
+
+export default Hero
