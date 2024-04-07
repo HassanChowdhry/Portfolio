42f66b58cc618b52937acec702ab22e91fb8cad2
Hassan Chowdhry <94680098+HassanChowdhry@users.noreply.github.com>
2024-04-07 00:37:17 -0300

    finished about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,6 +6,7 @@ import { motion } from 'framer-motion';
 import { styles } from '../style';
 import { services } from '../constants';
 import { fadeIn } from '../utils/motion'
+import { SectionWrapper } from '../hoc';
 
 const ServiceCard = ({index, title, icon }) => {
   return (
@@ -54,4 +55,4 @@ const About = () => {
   )
 }
 
-export default About
+export default SectionWrapper(About, "about")
